6c6d6a2540cab8552645c6351b8f4f5abc31e2c9
gbabatz <georgebabatz@gmail.com>
2020-04-01 20:27:34 +0300

    parameter file done

--- xml_tests/0_parameter_file_create.py
+++ xml_tests/0_parameter_file_create.py
@@ -19,19 +19,19 @@ def indent(elem, level=0):
 
 root = et.Element('paremeters')
 
-index_location = ' '
+index_location = '/indeces/index1'
 abs_path = '/home/gbabatz/workspace/IR/IR-2019-2020-Project-1'
-relative_bath = ['dtds','fbis','fr94','ft', 'latimes']
+relative_bath = ['/dtds','/fbis','/fr94','/ft', '/latimes']
 
 
 corpus = et.SubElement(root, 'corpus')
 
 for path in relative_bath:
-    et.SubElement(corpus, 'path').text = abs_path + '/' + path
+    et.SubElement(corpus, 'path').text = abs_path + path
     et.SubElement(corpus, 'class').text = 'trectext'
 
 
-et.SubElement(root, 'index').text = index_location
+et.SubElement(root, 'index').text = abs_path + index_location
 
 et.SubElement(root, 'memory').text = '2024M'
 et.SubElement(root, 'storeDocs').text = 'true'
@@ -40,5 +40,7 @@ et.SubElement(stemmer, 'name').text = 'Krovetz'
 
 indent(root)
 tree = et.ElementTree(root)
-tree.write('../parameter_file.parameter')
+tree.write('parameter_file.parameter')
+
+#print('done')
 
