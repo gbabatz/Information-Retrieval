db47deabb6d4dbf937e9b4308b6925d2955dbbc7
gbabatz <georgebabatz@gmail.com>
2020-04-04 19:08:18 +0300

    evaluation automated added

--- xml_tests/RunQueries_enhanced_automated.py
+++ xml_tests/RunQueries_enhanced_automated.py
@@ -3,9 +3,9 @@ import os
 
 def run_q(query_num):
     base_path = '/home/gbabatz/workspace/IR/IR-2019-2020-Project-1/'
-    basic_file_names = ['basic_queries_titles', 'basic_queries_titles_desc', 'basic_queries_titles_desc_narr']
+    basic_file_names = ['titles_wordnet', 'titles_desc_wordnet', 'titles_desc_narr_wordnet']
     query_path = base_path + basic_file_names[query_num] + '.xml'
-    save_results_path = base_path + 'results/basic/' + 'results_' + basic_file_names[query_num] + '.txt'
+    save_results_path = base_path + 'results/wordnet_enhanced/' + 'results_' + basic_file_names[query_num] + '.txt'
 
     command = 'IndriRunQuery ' + query_path + ' > ' + save_results_path
     os.system(command)
