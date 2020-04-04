2c4ee0c74c76053e87b63be5a92c3f43169921fb
gbabatz <georgebabatz@gmail.com>
2020-04-04 22:35:26 +0300

    minor change

--- xml_tests/basic_evaluation_automated.py
+++ xml_tests/basic_evaluation_automated.py
@@ -5,7 +5,7 @@ import os
 def run_eval(query_num):
     base_path = '/home/gbabatz/workspace/IR/IR-2019-2020-Project-1/'
     qrels_path = base_path + 'qrels.301-450.trec.adhoc'
-    basic_file_names = ['titles_wordnet', 'titles_desc_wordnet', 'titles_desc_narr_wordnet']
+    basic_file_names = ['basic_queries_titles', 'basic_queries_titles_desc', 'basic_queries_titles_desc_narr']
     query_results_path = base_path + 'results/basic/' + 'results_' + basic_file_names[query_num] + '.txt'
     save_results_path = base_path + 'results/basic/' + 'eval_' + basic_file_names[query_num] + '.txt'
 
